89e69dbcda01ecc8549f2a96eaae0f71358f8c98
Ankit Pandey <36405347+Ankitgkp@users.noreply.github.com>
2026-02-22 21:56:36 +0530

    Added next themes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-
-// todo --> add dark mode
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +25,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
